2c5e83096095a84d4fca129fd31b35993264035b
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-11-01 17:41:33 +0900

    anyway From Mac

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -2,9 +2,26 @@
 const num = 15;
 </script>
 
+
+
+
+<div>
+
+</div>
+
 <main>
 	<h1>Hello app {num}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>app8の派生をやる</p>
+	<p>コードはtag_and_descriptionの流用</p>
+	<p>具体的にはapp15はタグ管理できるシンプルなジョブボードを作る</p>
+	<p>登録無料利用無料で30秒で出稿できる</p>
+	<p>理由:自分に必要で自分以外の同業他社も必要としているため</p>
+	<p>メイン機能:タグとdescriptionのみのデータ構造</p>
+	<p>検索履歴をタグとして保存できる</p>
+	<p>求職者のニーズをそのままタグとして明文化/定量化できる</p>
+	<p>(タグを管理するメタタグが必要になるかも)</p>
+	<p>検索履歴のタグ化は保留していったん作って使ってもらって意見吸い上げるフォームつけとく(とりあえずgoogle form)</p>
+	<p>ver0.1は以上</p>
 </main>
 
 <style>
